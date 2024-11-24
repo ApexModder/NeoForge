1ab3cddc24ae75ae6648d919c0a47e27ac92ac40
XFactHD <xfacthd@gmx.de>
2024-11-24 21:18:38 +0100

    Break all the things

--- src/main/java/net/neoforged/neoforge/common/extensions/ITexturedModelExtension.java
+++ src/main/java/net/neoforged/neoforge/common/extensions/ITexturedModelExtension.java
@@ -5,31 +5,14 @@
 
 package net.neoforged.neoforge.common.extensions;
 
-import net.minecraft.client.renderer.block.model.BlockModel;
-import net.minecraft.client.renderer.block.model.ItemTransform;
+import net.minecraft.data.models.model.ModelTemplate;
 import net.minecraft.data.models.model.TexturedModel;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemDisplayContext;
+
+import java.util.function.UnaryOperator;
 
 public interface ITexturedModelExtension {
-    default TexturedModel withRenderType(String renderType) {
-        return new TexturedModel(self().getMapping(), self().getTemplate().withRenderType(renderType));
-    }
-
-    default TexturedModel withRenderType(ResourceLocation renderType) {
-        return new TexturedModel(self().getMapping(), self().getTemplate().withRenderType(renderType));
-    }
-
-    default TexturedModel withAmbientOcclusion(boolean ambientOcclusion) {
-        return new TexturedModel(self().getMapping(), self().getTemplate().withAmbientOcclusion(ambientOcclusion));
-    }
-
-    default TexturedModel withGuiLight(BlockModel.GuiLight guiLight) {
-        return new TexturedModel(self().getMapping(), self().getTemplate().withGuiLight(guiLight));
-    }
-
-    default TexturedModel withItemTransform(ItemDisplayContext displayContext, ItemTransform transform) {
-        return new TexturedModel(self().getMapping(), self().getTemplate().withItemTransform(displayContext, transform));
+    default TexturedModel updateTemplate(UnaryOperator<ModelTemplate> modifier) {
+        return new TexturedModel(self().getMapping(), modifier.apply(self().getTemplate()));
     }
 
     private TexturedModel self() {
@@ -37,24 +20,8 @@ public interface ITexturedModelExtension {
     }
 
     interface Provider {
-        default TexturedModel.Provider withRenderType(String renderType) {
-            return block -> self().get(block).withRenderType(renderType);
-        }
-
-        default TexturedModel.Provider withRenderType(ResourceLocation renderType) {
-            return block -> self().get(block).withRenderType(renderType);
-        }
-
-        default TexturedModel.Provider withAmbientOcclusion(boolean ambientOcclusion) {
-            return block -> self().get(block).withAmbientOcclusion(ambientOcclusion);
-        }
-
-        default TexturedModel.Provider withGuiLight(BlockModel.GuiLight guiLight) {
-            return block -> self().get(block).withGuiLight(guiLight);
-        }
-
-        default TexturedModel.Provider withItemTransform(ItemDisplayContext displayContext, ItemTransform transform) {
-            return block -> self().get(block).withItemTransform(displayContext, transform);
+        default TexturedModel.Provider updateTemplate(UnaryOperator<ModelTemplate> modifier) {
+            return block -> self().get(block).updateTemplate(modifier);
         }
 
         private TexturedModel.Provider self() {
